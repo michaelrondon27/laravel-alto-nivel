5e1d54d64f0a171b849cd7d54c5128297990eb1d
Michael Rondon <mrondon72@gmail.com>
2022-04-26 18:27:53 +0200

    18. Mostrando vistas como resultado de operaciones en Laravel

---
+++ laravel-desde-cero/resources/views/products/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>List of Products</h1>
+
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Soap</td>
+                        <td>Best soap ever</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Shampoo</td>
+                        <td>Best shampoo ever</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
---
+++ laravel-desde-cero/resources/views/products/show.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>Soap (1)</h1>
+
+        <p>Best soap ever</p>
+    </body>
+</html>
